13bc23ffa8a83ed973c2b9704417e7f959d83fea
System Administrator <root@Kaung.local>
2025-01-26 22:18:15 +0630

    Product CRUD

---
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Pages;
+
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\ProductResource;
+
+class CreateProduct extends CreateRecord
+{
+    protected static string $resource = ProductResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['created_by'] = auth()->id();
+        $data['updated_by'] = auth()->id();
+        return $data;
+    }
+}
